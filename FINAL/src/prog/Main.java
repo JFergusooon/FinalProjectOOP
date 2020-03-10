a7ce1bd009e737a62ed9c94201cb8431ea2520d6
ChazJanke <CJanke@student.neumont.edu>
2020-03-10 10:35:32 -0600

    fixed minor thing

--- FINAL/src/prog/Main.java
+++ FINAL/src/prog/Main.java
@@ -7,11 +7,11 @@ import models.*;
 public class Main {
 
     public static void main(String[] args) {
-        //Services.run();
-        Battle b = new Battle();
-        Player p = new Player("player", 50, true);
-        Npc n = new Npc("cpu", 50, true);
-        b.battleRun(p,n);
+        Services.run();
+//        Battle b = new Battle();
+//        Player p = new Player("player", 50, true);
+//        Npc n = new Npc("cpu", 50, true);
+//        b.battleRun(p,n);
 
 
     }
